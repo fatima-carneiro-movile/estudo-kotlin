8b8da49c502e786c76065675921e1dee6feb3e0b
Fatima Carneiro <fatima.carneiro@wavy.global>
2020-11-06 10:47:32 -0300

    Unversioned Files

---
+++ src/test/kotlin/com/fatimacarneiro/estudokotlin/EstudoKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fatimacarneiro.estudokotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EstudoKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
